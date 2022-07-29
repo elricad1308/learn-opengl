ca36f7f5548cec2c1d153207186e1842a0fd82f6
José Aguilar <jc.aguilar1308@gmail.com>
2022-07-29 12:45:13 -0500

    Agregada clase de Cámara

--- include/camera.h
+++ include/camera.h
@@ -85,13 +85,125 @@ public:
     WorldUp = glm::vec3(upX, upY, upZ);
     Yaw = yaw;
     Pitch = pitch;
-    updateCameraVector();
+    updateCameraVectors();
+  }
+
+  /**
+   * @brief Get the View Matrix object
+   * 
+   * @return glm::mat4 
+   */
+  glm::mat4 GetViewMatrix ()
+  {
+    return glm::lookAt(Position, Position + Front, Up);
+  }
+
+  /**
+   * @brief Process input received from any keyboard-like input system.
+   * 
+   * Accepts input parameter in the form of camera defined ENUM (to abs-
+   * tract it from windowing systems).
+   * 
+   * @param direction the input received
+   * 
+   * @param deltaTime time elapsed since the previous input signal
+   */
+  void ProcessKeyboard (Camera_Movement direction, float deltaTime)
+  {
+    float velocity = MovementSpeed * deltaTime;
+
+    if (direction == FORWARD)
+    {
+      Position += Front * velocity;
+    }
+
+    if (direction == BACKWARD)
+    {
+      Position -= Front * velocity;
+    }
+
+    if (direction == LEFT)
+    {
+      Position -= Right * velocity;
+    }
+
+    if (direction == RIGHT)
+    {
+      Position += Right * velocity;
+    }
+  }
+
+  /**
+   * @brief Process input received from a mouse input system.
+   * 
+   * Expects the offset value in both the X and Y directions.
+   * 
+   * @param xOffset change on the X axis since the last movement.
+   * 
+   * @param yOffset chance on the Y axis since the last movement.
+   * 
+   * @param constrainPitch whether or not the pitch shoudl be cons-
+   *   trained to avoid reaching 90 degrees both up and down. It helps
+   *   prevent gimball locks. Defaults to true
+   */
+  void ProcessMovement (float xOffset, float yOffset, GLboolean constrainPitch = true)
+  {
+    Yaw += xOffset * MouseSensitivity;
+    Pitch += yOffset * MouseSensitivity;
+
+    // If told so, constrain the pitch to avoid screen flipping
+    if (constrainPitch && Pitch > 89.0f)
+    {
+      Pitch = 89.0f;
+    }
+    if (constrainPitch && Pitch < -89.0f)
+    {
+      Pitch = -89.0f;
+    }
+
+    updateCameraVectors();
+  }
+
+  /**
+   * @brief Process input received from a mouse scroll-wheel event.
+   * 
+   * Only requires input on the vertical wheel axis.
+   * 
+   * @param yOffset 
+   */
+  void ProcessMouseScroll (float yOffset)
+  {
+    Zoom -= (float)yOffset;
+
+    if (Zoom < 1.0f)
+    {
+      Zoom = 1.0f;
+    }
+    if (Zoom > 45.0f)
+    {
+      Zoom = 45.0f;
+    }
   }
 
 private:
+/**
+ * @brief Calculates the front vector from the camera's Euler Angles
+ * 
+ */
   void updateCameraVectors()
   {
+    glm::vec3 front;
 
+    // Calculate the new Front vector
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+
+    Front = glm::normalize(front);
+
+    // Also recalculate the Right and Up vector
+    Right = glm::normalize(glm::cross(Front, WorldUp));
+    Up = glm::normalize(glm::cross(Right, Front));
   }
 };
 
