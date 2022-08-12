2a7f3493d19f9b4b6cbd9a54f2eda0a0dcc8459d
José Aguilar <jc.aguilar1308@gmail.com>
2022-08-11 19:51:28 -0500

    Completada lección 2 sobre iluminación

--- src/02-lighting/02b-basic-lighting.cpp
+++ src/02-lighting/02b-basic-lighting.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <shader_s.h>
-#include <camera.h>
+#include "../../include/camera.h"
 
 const int SCR_HEIGHT = 600;
 const int SCR_WIDTH = 800;
@@ -13,13 +17,63 @@ void mouse_callback (GLFWwindow *window, double xPosIn, double yPosIn);
 void process_input (GLFWwindow *window);
 void scroll_callback (GLFWwindow *window, double xOffset, double yOffset);
 
-bool firstMouse;
-float deltaTime, lastFrame, lastX, lastY;
-Camera camera(glm::vec3(0.0f, 0.0f, 0.3f));
+bool firstMouse = true;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
 int main ()
 {
+  float currentFrame;
+  float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+    -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+  };
+  unsigned int VBO, VAO[2];
   GLFWwindow *window;
+  glm::mat4 lightModel, model, view, projection;
+  glm::vec3 lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -34,7 +88,10 @@ int main ()
     return -1;
   }
   glfwMakeContextCurrent(window);
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
@@ -45,17 +102,81 @@ int main ()
   glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
   glEnable(GL_DEPTH_TEST);
 
+  glGenVertexArrays(2, VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(VAO[1]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  Shader objectShader("../shaders/textureless.vs.glsl", "../shaders/textureless.fs.glsl");
+  Shader lightShader("../shaders/light.vs.glsl", "../shaders/light.fs.glsl");
+
+  objectShader.use();
+  objectShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+  objectShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+  objectShader.setVec3("lightPos", lightPos);
+
+  lightModel = glm::mat4(1.0f);
+  lightModel = glm::translate(lightModel, lightPos);
+  lightModel = glm::scale(lightModel, glm::vec3(0.2f));
+
   while (!glfwWindowShouldClose(window))
   {
-    glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+    currentFrame = static_cast<float>(glfwGetTime());
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    
     process_input(window);
+
+    model = glm::mat4(1.0f);
+    view = camera.GetViewMatrix();
+    projection = glm::perspective(camera.Zoom, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    
+    objectShader.use();
+    objectShader.setMat4("model", model);
+    objectShader.setMat4("view", view);
+    objectShader.setMat4("projection", projection);
+    objectShader.setVec3("viewPos", camera.Position);
+    
+    glBindVertexArray(VAO[0]);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
+
+    lightShader.use();
+    lightShader.setMat4("model", lightModel);
+    lightShader.setMat4("view", view);
+    lightShader.setMat4("projection", projection);
+
+    glBindVertexArray(VAO[1]);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glBindVertexArray(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
   
+  lightShader.clear();
+  objectShader.clear();
+  glDeleteBuffers(1, &VBO);
+  glDeleteVertexArrays(2, VAO);
   glfwTerminate();
+
+  return 0;
 }
 
 void framebuffer_size_callback (GLFWwindow *window, int width, int height)
@@ -65,7 +186,23 @@ void framebuffer_size_callback (GLFWwindow *window, int width, int height)
 
 void mouse_callback (GLFWwindow *window, double xPosIn, double yPosIn)
 {
-  
+  float xPos = static_cast<float>(xPosIn);
+  float yPos = static_cast<float>(yPosIn);
+
+  if (firstMouse)
+  {
+    firstMouse = false;
+    lastX = xPos;
+    lastY = yPos;
+  }
+
+  float xOffset = xPos - lastX;
+  float yOffset = lastY - yPos;
+
+  lastX = xPos;
+  lastY = yPos;
+
+  camera.ProcessMouseMovement(xOffset, yOffset);
 }
 
 void process_input (GLFWwindow *window)
@@ -74,4 +211,39 @@ void process_input (GLFWwindow *window)
   {
     glfwSetWindowShouldClose(window, true);
   }
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+  {
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+  {
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+  {
+    camera.ProcessKeyboard(LEFT, deltaTime);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+  {
+    camera.ProcessKeyboard(RIGHT, deltaTime);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+  {
+    camera.ProcessKeyboard(UP, deltaTime);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+  {
+    camera.ProcessKeyboard(DOWN, deltaTime);
+  }
+}
+
+void scroll_callback (GLFWwindow *window, double xOffset, double yOffset)
+{
+  camera.ProcessMouseScroll(static_cast<float>(yOffset));
 }
--- src/shaders/textureless.fs.glsl
+++ src/shaders/textureless.fs.glsl
@@ -1,13 +1,29 @@
 #version 330 core
 
+in vec3 Normal;
+in vec3 FragPos;
+
 out vec4 FragColor;
 
+uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 viewPos;
 
 void main ()
 {
+  vec3 norm = normalize(Normal);
+  vec3 lightDir = normalize(lightPos - FragPos);
+  float diff = max(dot(norm, lightDir), 0.0);
+  vec3 diffuseLight = diff * lightColor;
+
+  float specularStrength = 0.5;
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 128);
+  vec3 specularLight = specularStrength * spec * lightColor;
+
   float ambientStrength = 0.1;
   vec3 ambientLight = ambientStrength * lightColor;    
-  FragColor = vec4(ambientLight * objectColor, 1.0);
+  FragColor = vec4((ambientLight + diffuseLight + specularLight) * objectColor, 1.0);
 }
